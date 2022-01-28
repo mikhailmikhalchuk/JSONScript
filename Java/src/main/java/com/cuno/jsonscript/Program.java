f3287610431e3b619a7ab28641a8c9536d7fab33
Mikhail Mikhalchuk <mikhailmcraft@gmail.com>
2022-01-28 17:34:01 -0500

    Java has been added

--- Java/src/main/java/com/cuno/jsonscript/Program.java
+++ Java/src/main/java/com/cuno/jsonscript/Program.java
@@ -10,12 +10,18 @@ import javax.tools.ToolProvider;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Program {
     public static String FilesDirectory = new JFileChooser().getFileSystemView().getDefaultDirectory().toString() + "/JSONScript/Java";
 
     public static void main(String[] args) {
+        FileHelper.ValidAccessModifiers.add("public");
+        FileHelper.ValidAccessModifiers.add("private");
+        FileHelper.ValidAccessModifiers.add("protected");
+        FileHelper.ValidAccessModifiers.add("none");
         FileCompilerSettings compilerSettings = new FileCompilerSettings();
         compilerSettings.SilentCompilation = false;
         if (!Files.exists(Path.of(FilesDirectory + "/Compiler"))) {
@@ -26,7 +32,50 @@ public class Program {
                 JSONObject compilerJson = new JSONObject();
                 compilerJson.put("SilentCompilation", false);
                 FileHelper.QuickWrite(FilesDirectory + "/compilerSettings.json", compilerJson.toString(2));
+
+                JSONObject exampleClass = new JSONObject();
+                List<String> imports = new ArrayList<>();
+                imports.add("java.io.*");
+                imports.add("java.util.List");
+                exampleClass.put("Imports", imports);
+                exampleClass.put("AccessModifier", "public");
+                exampleClass.put("Namespace", "com.compiler.jsonscript");
+                exampleClass.put("Name", "Program");
+                FileHelper.QuickWrite(FilesDirectory + "/Class-Program.json", exampleClass.toString(2));
+
+                JSONObject exampleMethod = new JSONObject();
+                List<String> paramNames = new ArrayList<>();
+                List<String> paramTypes = new ArrayList<>();
+                paramNames.add("args");
+                paramTypes.add("String[]");
+                exampleMethod.put("ParameterTypes", paramTypes);
+                exampleMethod.put("ParameterNames", paramNames);
+                exampleMethod.put("Code", "System.out.println(\"Testing...\");\nexampleMethod(5, false);");
+                exampleMethod.put("IsStatic", true);
+                exampleMethod.put("ContainingClass", "Program");
+                exampleMethod.put("AccessModifier", "public");
+                exampleMethod.put("Name", "main");
+                FileHelper.QuickWrite(FilesDirectory + "/Method-Main.json", exampleMethod.toString(2));
+
+                JSONObject exampleMethod2 = new JSONObject();
+                List<String> paramNames2 = new ArrayList<>();
+                List<String> paramTypes2 = new ArrayList<>();
+                paramNames2.add("ex");
+                paramTypes2.add("int");
+                paramNames2.add("ex2");
+                paramTypes2.add("boolean");
+                exampleMethod2.put("ParameterTypes", paramTypes2);
+                exampleMethod2.put("ParameterNames", paramNames2);
+                exampleMethod2.put("Code", "return 3;");
+                exampleMethod2.put("IsStatic", true);
+                exampleMethod2.put("ReturnType", "int");
+                exampleMethod2.put("ContainingClass", "Program");
+                exampleMethod2.put("AccessModifier", "private");
+                exampleMethod2.put("Name", "exampleMethod");
+                FileHelper.QuickWrite(FilesDirectory + "/Method-ExampleMethod.json", exampleMethod2.toString(2));
+
                 System.out.println("Done!");
+                return;
             }
             catch (IOException e) {
 
@@ -41,14 +90,11 @@ public class Program {
             System.out.println("Looking for JSON files.");
         }
 
+        List<FileClassSettings> AllClasses = new ArrayList<>();
+        List<FileMethodSettings> AllMethods = new ArrayList<>();
+        int ignoredFiles = 0;
+
         String masterCode = """
-                package com.compiler.jsonscript;
-                
-                public class Program {
-                    public static void main(String[] args) {
-                        System.out.println("Test");
-                    }
-                }
                 """;
         File folder = new File(FilesDirectory);
         File[] jsonList = folder.listFiles();
@@ -59,22 +105,71 @@ public class Program {
                 continue;
             }
             else {
-                if (file.getName().toLowerCase().contains("Method")) {
-
+                if (file.getName().toLowerCase().contains("class")) {
+                    AllClasses.add(FileHelper.DeserializeClass(file.getAbsolutePath()));
+                }
+                else if (file.getName().toLowerCase().contains("method")) {
+                    AllMethods.add(FileHelper.DeserializeMethod(file.getAbsolutePath()));
+                }
+                else if (!file.getName().equals("compilerSettings.json")) {
+                    ignoredFiles++;
                 }
             }
         }
 
         if (!compilerSettings.SilentCompilation) {
-            System.out.println("Done!");
-            System.out.println("Writing program to temporary file...");
+            System.out.println(String.format("Found %s class%s.", AllClasses.size(), AllClasses.size() != 1 ? "es" : ""));
+            System.out.println(String.format("Found %s method%s.", AllMethods.size(), AllMethods.size() != 1 ? "s" : ""));
+            System.out.println(String.format("(%s file%s ignored)", ignoredFiles, ignoredFiles != 1 ? "s were" : " was"));
+            System.out.println("----\nAssigning methods to classes...");
+        }
+
+        int nonIgnoredMethods = 0;
+
+        for (int i = 0; i < AllClasses.size(); i++) {
+            FileClassSettings settings = AllClasses.get(i);
+            String masterMethods = "";
+            for (int j = 0; j < AllMethods.size(); j++) { //most efficient method
+                FileMethodSettings childMethod = AllMethods.get(j);
+                if (childMethod.ContainingClass.equals(settings.Name)) {
+                    nonIgnoredMethods++;
+                    String params = "";
+                    for (int k = 0; k < childMethod.ParameterNames.size(); k++) { //system.linq
+                        if (params == "") {
+                            params += childMethod.ParameterTypes.get(k) + " " + childMethod.ParameterNames.get(k);
+                        }
+                        else {
+                            params += ", " + childMethod.ParameterTypes.get(k) + " " + childMethod.ParameterNames.get(k);
+                        }
+                    }
+                    masterMethods += String.format("""
+                                %s %s %s %s(%s) {
+                                    %s
+                                }
+                            """, childMethod.AccessModifier != "none" ? childMethod.AccessModifier : "", childMethod.IsStatic ? "static" : "", childMethod.ReturnType, childMethod.Name, params, childMethod.Code);
+                }
+            }
+
+            masterCode += String.format("""
+                    package %s;
+                    \n
+                    %s class %s {
+                        %s
+                    }
+                    """, settings.Namespace, settings.AccessModifier != "none" ? settings.AccessModifier : "", settings.Name, masterMethods);
+            //System.out.println(masterCode);
+        }
+
+        if (!compilerSettings.SilentCompilation) {
+            System.out.println(String.format("Done! (%s method%s ignored)", AllMethods.size() - nonIgnoredMethods, AllMethods.size() - nonIgnoredMethods != 1 ? "s were" : " was"));
+            System.out.println("----\nWriting program to temporary file...");
         }
 
         FileHelper.QuickWrite(FilesDirectory + "/Compiler/Program.java", masterCode);
 
         if (!compilerSettings.SilentCompilation) {
             System.out.println("Done!");
-            System.out.println("Running compiler...");
+            System.out.println("----\nRunning compiler...");
         }
 
         JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
@@ -83,7 +178,7 @@ public class Program {
 
         if (success == 0 && !compilerSettings.SilentCompilation) {
             System.out.println("Done!");
-            System.out.println("Running compiled file...\n--------");
+            System.out.println("----\nRunning compiled file...\n--------");
             Runtime rt = Runtime.getRuntime();
             try {
                 Process pr = rt.exec("java " + FilesDirectory + "/Compiler/Program.java");
@@ -106,23 +201,5 @@ public class Program {
                 throw new RuntimeException("Running compiled file failed: " + e.getMessage());
             }
         }
-
-        /*try {
-            File file = new File("C:/Users/Cuno/Documents/JSONScript/Java/amogus.json");
-            Scanner myReader = new Scanner(file);
-            String data = "";
-            while (myReader.hasNextLine()) {
-                data += myReader.nextLine();
-            }
-            JSONObject obj = new JSONObject(data);
-            String test = obj.getString("AccessModifier");
-            System.out.println(test);
-            JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-            String fileToCompile = "sus";
-            //Object sus = compiler.run(null, null, null, fileToCompile);
-        }
-        catch (FileNotFoundException e) {
-            System.out.println("Bad!");
-        }*/
     }
 }
--- Java/src/main/java/com/cuno/jsonscript/common/FileHelper.java
+++ Java/src/main/java/com/cuno/jsonscript/common/FileHelper.java
@@ -2,9 +2,110 @@ package com.cuno.jsonscript.common;
 
 import org.json.*;
 import java.io.*;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class FileHelper {
+
+    public static List<String> ValidAccessModifiers = new ArrayList<>();
+
+    public static FileClassSettings DeserializeClass(String filePath) {
+        FileClassSettings classSettings = new FileClassSettings();
+        try {
+            File file = new File(filePath);
+            Scanner myReader = new Scanner(file);
+            String data = "";
+            while (myReader.hasNextLine()) {
+                data += myReader.nextLine();
+            }
+            JSONObject obj = new JSONObject(data);
+            classSettings.Name = obj.getString("Name");
+            if (classSettings.Name == null) {
+                throw new IllegalArgumentException("A name must be provided for class in file \"" + file.getName() + "\"");
+            }
+            classSettings.Namespace = obj.getString("Namespace");
+            if (classSettings.Namespace == null) {
+                throw new IllegalArgumentException("A namespace must be provided for class in file \"" + file.getName() + "\"");
+            }
+            classSettings.AccessModifier = obj.getString("AccessModifier").toLowerCase();
+            if (!ValidAccessModifiers.contains(classSettings.AccessModifier)) {
+                throw new IllegalArgumentException(String.format("Provided access modifier \"%s\" is not a valid access modifier (class %s in %s)", classSettings.AccessModifier, classSettings.Name, classSettings.Namespace));
+            }
+            classSettings.Imports = ((JSONArray) obj.get("Imports")).toList();
+        }
+        catch (FileNotFoundException e) {
+            throw new RuntimeException(String.format("Failed to read file %s: Could not find file.", filePath));
+        }
+        return classSettings;
+    }
+
+    public static FileMethodSettings DeserializeMethod(String filePath) {
+        FileMethodSettings methodSettings = new FileMethodSettings();
+        try {
+            File file = new File(filePath);
+            Scanner myReader = new Scanner(file);
+            String data = "";
+            while (myReader.hasNextLine()) {
+                data += myReader.nextLine();
+            }
+            JSONObject obj = new JSONObject(data);
+            try { //greatest library
+                methodSettings.Name = obj.getString("Name");
+                methodSettings.ContainingClass = obj.getString("ContainingClass");
+                methodSettings.AccessModifier = obj.getString("AccessModifier").toLowerCase();
+                if (!ValidAccessModifiers.contains(methodSettings.AccessModifier)) {
+                    throw new IllegalArgumentException(String.format("Provided access modifier \"%s\" is not a valid access modifier (method %s in %s)", methodSettings.AccessModifier, methodSettings.Name, methodSettings.ContainingClass));
+                }
+                methodSettings.Code = obj.getString("Code");
+            }
+            catch (JSONException e) {
+                if (e.getMessage().contains("[\"Name\"]")) {
+                    throw new IllegalArgumentException("A name must be provided for method in file \"" + file.getName() + "\"");
+                }
+                if (e.getMessage().contains("[\"ContainingClass\"]")) {
+                    throw new IllegalArgumentException("A container class must be provided for method in file \"" + file.getName() + "\"");
+                }
+                if (e.getMessage().contains("[\"AccessModifier\"]")) {
+                    throw new IllegalArgumentException("An access modifier must be provided for method in file \"" + file.getName() + "\"");
+                }
+                if (e.getMessage().contains("[\"Code\"]")) {
+                    throw new IllegalArgumentException("Code to run upon method invocation must be provided for method in file \"" + file.getName() + "\"");
+                }
+            }
+
+            try {
+                methodSettings.IsStatic = obj.getBoolean("IsStatic");
+            }
+            catch (JSONException e) {
+                methodSettings.IsStatic = false;
+            }
+
+            try {
+                methodSettings.ReturnType = obj.getString("ReturnType");
+            }
+            catch (JSONException e) {
+                methodSettings.ReturnType = "void";
+            }
+            try {
+                methodSettings.ParameterNames = ((JSONArray) obj.get("ParameterNames")).toList();
+                methodSettings.ParameterTypes = ((JSONArray) obj.get("ParameterTypes")).toList();
+            }
+            catch (JSONException e) {
+                methodSettings.ParameterNames = new ArrayList<>();
+                methodSettings.ParameterTypes = new ArrayList<>();
+            }
+            if (methodSettings.ParameterTypes.size() != methodSettings.ParameterNames.size()) {
+                throw new InputMismatchException(String.format("Mismatch in size of ParameterNames and ParameterTypes: both arrays must have the same amount of values (method %s in %s)", methodSettings.Name, methodSettings.ContainingClass));
+            }
+        }
+        catch (FileNotFoundException e) {
+            throw new RuntimeException(String.format("Failed to read file %s: Could not find file.", filePath));
+        }
+        return methodSettings;
+    }
 
     public static FileCompilerSettings DeserializeCompiler(String filePath) {
         FileCompilerSettings compilerSettings = new FileCompilerSettings();
@@ -33,6 +134,34 @@ public class FileHelper {
         catch (IOException e) {
             throw new RuntimeException(String.format("Failed to write to file %s: %s", filePath, e.getMessage()));
         }
+    }
+
+    public static class FileClassSettings {
+        public String Name;
+
+        public String AccessModifier;
+
+        public String Namespace;
+
+        public List<Object> Imports;
+    }
+
+    public static class FileMethodSettings {
+        public String Name;
+
+        public String AccessModifier;
+
+        public String ContainingClass;
+
+        public String ReturnType = "void";
+
+        public boolean IsStatic;
+
+        public List<Object> ParameterNames;
+
+        public List<Object> ParameterTypes;
+
+        public String Code;
     }
 
     public static class FileCompilerSettings {
